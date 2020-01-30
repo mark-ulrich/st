a473b261951740e745fc2a2d2daba2b8b777aee2
Mark Ulrich <mark.ulrich.86@gmail.com>
2020-01-30 14:59:47 -0600

    Switched to 'Deafened' color scheme (terminal.sexy)

--- config.h
+++ config.h
@@ -84,42 +84,50 @@ unsigned int tabspaces = 4;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+  /* 8 normal colors */
+  [0] = "#3d3e3d", /* black   */
+  [1] = "#755a5b", /* red     */
+  [2] = "#68755a", /* green   */
+  [3] = "#756e5a", /* yellow  */
+  [4] = "#5b6976", /* blue    */
+  [5] = "#755b76", /* magenta */
+  [6] = "#465457", /* cyan    */
+  [7] = "#ccccc6", /* white   */
 
-	[255] = 0,
+  /* 8 bright colors */
+  [8]  = "#5a5b5c", /* black   */
+  [9]  = "#a37679", /* red     */
+  [10] = "#87a376", /* green   */
+  [11] = "#a39b76", /* yellow  */
+  [12] = "#758ba3", /* blue    */
+  [13] = "#9f76a3", /* magenta */
+  [14] = "#899ca1", /* cyan    */
+  [15] = "#f8f8f2", /* white   */
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+  /* special colors */
+  [256] = "#000000", /* background */
+  [257] = "#ffffff", /* foreground */
 };
-
 
 /*
  * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
+ * foreground, background, cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
+
+/*
+ * Colors used, when the specific fg == defaultfg. So in reverse mode this
+ * will reverse too. Another logic would only make the simple feature too
+ * complex.
+ */
+static unsigned int defaultitalic = 7;
+static unsigned int defaultunderline = 7;
+
 static unsigned int defaultrcs = 257;
+
 float alpha = 0.93f;
 float alphaUnfocussed = 0.85f;
 
