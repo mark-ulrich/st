85f6c4476f5d1e36fbc9c6781287320e44067f83
Mark Ulrich <mark.ulrich.86@gmail.com>
2020-02-13 04:55:18 -0600

    Changed font size

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "TerminessTTFNerdFont:pixelsize=18:antialias=true:autohint=true";
+static char *font = "TerminessTTFNerdFontMono:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
