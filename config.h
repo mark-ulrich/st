7655ad32e80a270ca0caba098a6312509edcbeb2
Mark Ulrich <mark.ulrich.86@gmail.com>
2020-03-23 23:47:44 -0500

    Changed default dimensions

--- config.h
+++ config.h
@@ -167,8 +167,8 @@ static unsigned int cursorshape = 2;
  * Default columns and rows numbers
  */
 
-static unsigned int cols = 80;
-static unsigned int rows = 24;
+static unsigned int cols = 110;
+static unsigned int rows = 35;
 
 /*
  * Default colour and shape of the mouse cursor
