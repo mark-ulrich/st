9928d8a0a792b15090cbbf421b959228e7a83ca2
Mark Ulrich <mark.ulrich.86@gmail.com>
2020-07-04 02:02:36 -0500

    Added Alt+p/n hotkeys for scrolling

--- config.h
+++ config.h
@@ -199,7 +199,8 @@ static MouseShortcut mshortcuts[] = {
 };
 
 /* Internal keyboard shortcuts. */
-#define MODKEY Mod1Mask
+#define MODKEY  Mod1Mask
+#define AltMask Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
 
 static Shortcut shortcuts[] = {
@@ -218,6 +219,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ AltMask,              XK_n,           kscrolldown,    {.i = -1} },
+	{ AltMask,              XK_p,           kscrollup,      {.i = -1} },
 };
 
 /*
